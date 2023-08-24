62566676d8e8357f6ba2fd69d7aecd1868132f3f
prarthana-km <143058612+prarthana-km@users.noreply.github.com>
2023-08-24 10:01:48 +0530

    main
    hello

---
+++ main.js
@@ -0,0 +1,12 @@
+let name2 = "admin"
+let num1 = 22
+let num2 = 33
+let sum 
+sum = num1 + num2
+console.log(name2)
+console.log(sum)
+let textbox
+document.getElementById("btn").onclick=function()
+    console.log("button clicked")
+    box=document.getElementById("txt").Value
+    console.log(textbox)
